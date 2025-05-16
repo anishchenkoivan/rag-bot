98fedd3dba2b3a1fa6a9167c9a2f5bd7b45bc57e
Ivan Anishchenko <ivananisenko08@gmail.com>
2025-05-16 23:37:42 +0300

    Add image recognition support

--- bot/service/data_extractor.py
+++ bot/service/data_extractor.py
@@ -8,10 +8,10 @@ def extract_data(data, source: str) -> str:
 
     file_path = data
     file_ext = Path(file_path).suffix.lower()
-    if file_ext in (".txt", "docx", ""):
+    if file_ext in (".txt", ".docx", ""):
         with open(file_path, "r", encoding="utf-8") as f:
             return f.read()
-    if file_ext in ("jpg", "jpeg", "png"):
+    if file_ext in (".jpg", ".jpeg", ".png"):
         return text_from_image(file_path)
     return ""
     # TODO: Add proper text/image recognition from PDF
