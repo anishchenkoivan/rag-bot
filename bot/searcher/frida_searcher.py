e240f58ea73aad2efdf8a6e0e6fd12ee52101ff7
Ivan Anishchenko <ivananisenko08@gmail.com>
2025-05-17 02:20:23 +0300

    Optimizations

--- bot/searcher/frida_searcher.py
+++ bot/searcher/frida_searcher.py
@@ -10,25 +10,23 @@ from .searcher import Searcher
 
 class FridaSearcher(Searcher):
     def __init__(self, segments: list[str]):
+        super().__init__(segments)
         device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model = SentenceTransformer("ai-forever/FRIDA", device=device)
-        super().__init__(segments)
+
+        search_docs = [f"search_document: {seg}" for seg in self.segments]
+        self.doc_embeddings = self.model.encode(search_docs, convert_to_tensor=True)
 
     def retrieve_answers(self, questions, limit=2) -> list[str]:
         if len(self.segments) == 1:
             return [self.segments[0]] # Otherwise funny things happen in torch
 
-        search_docs = [f"search_document: {seg}" for seg in self.segments]
         search_query = [f"search_query: {q}" for q in questions]
-        all_inputs = search_docs + search_query
-
-        embeddings = self.model.encode(all_inputs, convert_to_tensor=True)
-        doc_embeddings = embeddings[:len(self.segments)]
-        query_embeddings = embeddings[len(self.segments):]
+        query_embeddings = self.model.encode(search_query, convert_to_tensor=True)
 
         answers = []
         for query_embedding in query_embeddings:
-            sim_scores = (query_embedding @ doc_embeddings.T).squeeze(0)
+            sim_scores = (query_embedding @ self.doc_embeddings.T).squeeze(0)
             _, topk_indices = torch.topk(sim_scores, k=limit)
             top_segments = ";".join([self.segments[i] for i in topk_indices])
             answers.append(top_segments)
